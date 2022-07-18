9d31d717132c35f4f78e83069bb1bd2f660d8241
Takahisa99 <106425553+Takahisa99@users.noreply.github.com>
2022-07-18 11:14:31 +0000

    ジャンル機能追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
   config.hosts << "9288950852eb4dec9d6b1c63103c1c2c.vfs.cloud9.us-east-1.amazonaws.com"
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "cb8e5198043945a189257a2a16b95f50.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
