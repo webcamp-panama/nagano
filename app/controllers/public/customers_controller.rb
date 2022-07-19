c3c48045916f6a2a8e69e5265324d2160af44bd8
konnotakafumi4 <longwenjinye@gmail.com>
2022-07-19 10:14:32 +0000

    [Add]顧客のマイページ、登録情報編集画面

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,14 +4,25 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_order_path, notice: "You have updated user successfully."
+    else
+      render 'edit'
+    end
   end
 
   def unsbscribe
   end
 
   def withdrawal
+  end
+
+  def customer_params
+    params.require(:customer).permit(:email, :last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number)
   end
 end
