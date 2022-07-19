9b9f297ffa2887ad098e5ff3e7ecc9cb87506cb1
moneeeet <t.o.p11040809@gmail.com>
2022-07-19 14:48:55 +0000

    顧客画面途中

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -14,6 +14,10 @@ class Admin::CustomersController < ApplicationController
   end
 
   def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      redirect_to admin_customer_path(@customer)
+    end
   end
 
    private
