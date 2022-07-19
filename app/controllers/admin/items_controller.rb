5249ef66a0db875b1cb05d62da0cfef9bb68324a
Takahisa99 <106425553+Takahisa99@users.noreply.github.com>
2022-07-19 10:17:11 +0000

    管理者側の商品画面

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,19 +1,54 @@
 class Admin::ItemsController < ApplicationController
   def index
+    @items = Item.all
+    @item = Item.new
+    @item_page = Item.page(params[:page])
   end
 
   def new
+    @item = Item.new
+    @items = Item.all
   end
 
   def create
+    @items = Item.all
+    @item = Item.new(item_params)
+    if @item.save
+       redirect_to admin_items_path(@item)
+    else
+       render :index
+    end
   end
+
+
 
   def show
+    @item = Item.find(params[:id])
   end
+
+
 
   def edit
+    @item = Item.find(params[:id])
   end
 
+
+
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_items_path
+    else
+      render :edit
+    end
   end
+
+  def item_params
+    params.require(:item).permit(:name, :price, :introduction)
+  end
+
+
+
 end
+
+
