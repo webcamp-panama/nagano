1b1aa2f3f8ada9e7851a8d20540746ad696cc779
Natty0404 <106375669+Natty0404@users.noreply.github.com>
2022-07-19 13:22:10 +0000

    [Add] enum/注文ステータス

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ApplicationRecord
   enum payment_method: { credit_card: 0, transfer: 1 }
+  enum status: { payment_waiting: 0, payment_confirm: 1, making: 2, prepare_shipping: 3, shipping_out: 4 }
+
 end
