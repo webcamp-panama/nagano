c22504cd6ef6c81ac2de53484e86959931b3028f
Natty0404 <106375669+Natty0404@users.noreply.github.com>
2022-07-19 05:07:00 +0000

    [Update] admin/sign_in

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Admin::SessionsController < Devise::SessionsController
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
   protected
     def after_sign_in_path_for(resource)
       admin_items_path
@@ -9,4 +11,9 @@ class Admin::SessionsController < Devise::SessionsController
     def after_sign_out_path_for(resource)
       new_admin_session_path
     end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
+    end
+
 end
