e5bbdc549aa1beda5b8e40d2d148a42bcbd88dad
Takahisa99 <106425553+Takahisa99@users.noreply.github.com>
2022-07-21 08:18:00 +0000

    更新

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,24 @@
 class Admin::OrdersController < ApplicationController
+
   def show
+    @order = Order.find(params[:id])
   end
 
   def update
+    @order = Order.find(params[:id])
+    if @order.update(order_params)
+      redirect_to admin_order_path
+    else
+      render :show
+    end
   end
+
+
+  def order_params
+    params.require(:order).permit(:customer_id , :postcode, :address, :name, :payment_method, :total_payment, :shipping_cost, :status )
+  end
+
+
+
+
 end
