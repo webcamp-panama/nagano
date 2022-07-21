dec9ccd9d959004116ef66da09c6fab37d4acdc4
Takahisa99 <106425553+Takahisa99@users.noreply.github.com>
2022-07-21 09:09:27 +0000

    更新

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,13 +10,17 @@ class Item < ApplicationRecord
       (price * 1.1).floor
   end
 
-  def get_image(width, height)
-  unless image.attached?
-    file_path = Rails.root.join('app/assets/images/cake_about.jpg')
-    image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  def add_tax_on_price
+   (self.price * 1.10).round
   end
-  image.variant(resize_to_limit: [width, height]).processed
-  end
+
+  # def get_image(width, height)
+  # unless image.attached?
+  #   file_path = Rails.root.join('app/assets/images/cake_about.jpg')
+  #   image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+  # end
+  # image.variant(resize_to_limit: [width, height]).processed
+  # end
 
 
 
