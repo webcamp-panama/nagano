85d847839f1c6c0d953ba23945649326127e4e1f
Natty0404 <106375669+Natty0404@users.noreply.github.com>
2022-07-22 09:17:23 +0000

    [Update] image method

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,8 +15,8 @@ class Item < ApplicationRecord
 
    def get_image(width, height)
     unless image.attached?
-     file_path = Rails.root.join('app/assets/images/cake_about.jpg')
-     image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+     file_path = Rails.root.join('app/assets/images/no_image.png')
+     image.attach(io: File.open(file_path), filename: 'default-image.png', content_type: 'image/jpeg')
     end
     image.variant(resize_to_limit: [width, height]).processed
    end
