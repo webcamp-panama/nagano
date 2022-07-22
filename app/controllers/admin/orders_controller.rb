63a3ea324808b3e6b3b6b0ca437924482e3aee1c
Takahisa99 <106425553+Takahisa99@users.noreply.github.com>
2022-07-22 08:25:01 +0000

    管理者側の注文履歴詳細の追加（途中）

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,6 +5,10 @@ class Admin::OrdersController < ApplicationController
     #@order = Order.find(params[:id])
   end
 
+
+
+
+
   def update
     @order = Order.find(params[:id])
     if @order.update(order_params)
@@ -15,8 +19,17 @@ class Admin::OrdersController < ApplicationController
   end
 
 
+  def confirm
+    @order = Order.new(order_params)
+    @address = Address.find(params[:order][:address_id])
+    @order.postal_code = @address.postal_code
+    @order.address = @address.address
+    @order.name = @address.name
+  end
+
+
   def order_params
-    params.require(:order).permit(:customer_id , :postcode, :address, :name, :payment_method, :total_payment, :shipping_cost, :status )
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
   end
 
 
--- app/models/item.rb
+++ app/models/item.rb
@@ -24,6 +24,9 @@ class Item < ApplicationRecord
   def add_tax_on_price
    (self.price * 1.10).round
   end
+
+
+end
 # <<<<<<< HEAD
 
 #   def get_image(width, height)
