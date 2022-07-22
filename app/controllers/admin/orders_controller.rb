2bad7b5922447099590ad97ac4cf2169d6ed5a85
konnotakafumi4 <longwenjinye@gmail.com>
2022-07-22 12:08:43 +0000

    カート、注文履歴詳細　レイアウト調整

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,10 +5,6 @@ class Admin::OrdersController < ApplicationController
     #@order = Order.find(params[:id])
   end
 
-
-
-
-
   def update
     @order = Order.find(params[:id])
     if @order.update(order_params)
@@ -18,7 +14,6 @@ class Admin::OrdersController < ApplicationController
     end
   end
 
-
   def confirm
     @order = Order.new(order_params)
     @address = Address.find(params[:order][:address_id])
@@ -27,11 +22,8 @@ class Admin::OrdersController < ApplicationController
     @order.name = @address.name
   end
 
-
   def order_params
     params.require(:order).permit(:payment_method, :postal_code, :address, :name)
   end
-
-
 
 end
