8c711465dc8a59a7e6f9e823cb7cfd907111c019
moneeeet <t.o.p11040809@gmail.com>
2022-07-23 11:10:05 +0000

    注文確定処理機能追加

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -36,6 +36,7 @@ class Public::OrdersController < ApplicationController
   def create
     @orders = Order.all
     @order = Order.new
+    redirect_to public_orders_complete_path
   end
 
   def index
