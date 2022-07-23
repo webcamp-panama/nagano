77f90861dd52dfb1d304703de58b8a417dcdcc0c
moneeeet <t.o.p11040809@gmail.com>
2022-07-23 07:20:37 +0000

    errorsolved

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,10 +1,15 @@
 class Public::OrdersController < ApplicationController
   def new
     @order = Order.new
+
   end
 
   def confirm
-    @order = Order.find(params[:id])
+    @order = Order.new(order_params)
+    @delivery = Delivery.find(params[:order][:address_id])
+    @order.postal_code = @delivery.postcode
+    @order.address = @delivery.address
+    @order.name = @delivery.name
   end
 
   def complete
@@ -12,7 +17,11 @@ class Public::OrdersController < ApplicationController
 
   def create
     @orders = Order.all
-    redirect_to public_orders_confirm_path
+    @order = Order.new(order_params)
+
+      redirect_to public_orders_confirm_path
+
+
   end
 
   def index
@@ -22,8 +31,9 @@ class Public::OrdersController < ApplicationController
   def show
   end
 
+ private
   def order_params
-    params.require(:order).permit(:payment_method, :postal_code, :address, :name)
+    params.require(:order).permit(:payment_method,  :postcode, :address, :name, :address_select, :address_id)
   end
 
 
