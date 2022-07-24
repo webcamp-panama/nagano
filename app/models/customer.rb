5d11295d6f47461a44ed9cf28003665a59ee4f39
moneeeet <t.o.p11040809@gmail.com>
2022-07-24 02:24:26 +0000

    update

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -8,15 +8,15 @@ class Customer < ApplicationRecord
   has_many :deliveries
 
   def full_name
-    self.last_name + " " + self.first_name
+    self.first_name + " " + self.last_name
   end
 
   def full_name
-    self.last_name + " " + self.first_name
+    self.first_name + " " + self.last_name
   end
 
   def full_name_kana
-    self.last_name_kana + " " + self.first_name_kana
+    self.first_name_kana + " " + self.last_name_kana
   end
 
   def active_for_authentication?
