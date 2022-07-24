b5ecadfe4c8b424b9ba0db5c19a80e829cd85b50
moneeeet <t.o.p11040809@gmail.com>
2022-07-24 02:42:59 +0000

    update

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -11,10 +11,6 @@ class Customer < ApplicationRecord
     self.first_name + " " + self.last_name
   end
 
-  def full_name
-    self.first_name + " " + self.last_name
-  end
-
   def full_name_kana
     self.first_name_kana + " " + self.last_name_kana
   end
