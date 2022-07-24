30082e5bc27ccb6d02d90b68bb4a1f4069a36c4e
Natty0404 <106375669+Natty0404@users.noreply.github.com>
2022-07-24 08:50:09 +0000

    [Finish] public/index,show

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,9 +4,9 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :cart_items
-  has_many :deliveries
-  has_many :orders
+  has_many :cart_items, dependent: :destroy
+  has_many :deliveries, dependent: :destroy
+  has_many :orders, dependent: :destroy
 
   def full_name
     self.last_name + " " + self.first_name
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,5 +1,11 @@
 class OrderDetail < ApplicationRecord
     belongs_to :order
     belongs_to :item
-    
+
+    enum making_status: { not_startable: 0, waiting_making: 1, in_making: 2, making_complete: 3 }
+
+     def subtotal
+     (price * 1.1).floor * quantity
+     end
+
 end
